883be9e812d2e22aceead97c1058d17ba1ad7e19
CHOCOLATEWIN\Administrator <2926436191@qq.com>
2026-04-19 15:39:12 +0800

    feat: 跑通git pr ai评论

--- Assets/Scripts/Sample.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class Sample : MonoBehaviour
-{
-    void Update()
-    {
-        var rb = GetComponent<Rigidbody>();
-        Debug.Log("Pos: " + transform.position.ToString());
-    }
-}
